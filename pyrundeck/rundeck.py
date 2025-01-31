8fd1418148a54f6ec485e94decaa42a0d758fed0
Chris Adams <cadams@entrata.com>
2025-01-31 13:51:56 -0700

    Add endpoints

--- pyrundeck/rundeck.py
+++ pyrundeck/rundeck.py
@@ -45,7 +45,7 @@ class Rundeck(object):
         return r.cookies["JSESSIONID"]
 
     def __request(
-        self, method, url, params=None, upload_file=None, format="json"
+        self, method, url, params=None, upload_file=None, get_file_path=None, format="json"
     ):
         logger.info("{} {} Params: {}".format(method, url, params))
         cookies = dict()
@@ -62,25 +62,34 @@ class Rundeck(object):
             "headers": h,
             "verify": self.verify,
         }
-        if method == "GET":
+        if "GET" in method:
             options["params"] = params
         elif upload_file is not None:
+            options["params"] = params
             options["data"] = upload_file
-            options["headers"]["Content-Type"] = "octet/stream"
+            # options["headers"]["Content-Type"] = "octet/stream"
         else:
             options["json"] = params
 
-        r = requests.request(method, url, **options)
-        logger.debug(r.text)
-        r.raise_for_status()
-        if format == "json":
-            try:
-                return r.json()
-            except ValueError as e:
-                logger.error(e)
-                return r.text
+        if method == 'GET_FILE':
+            r = requests.get(url, stream=True, **options)
+            with open(get_file_path, "wb") as f:
+                for chunk in r.iter_content(chunk_size=512):
+                    if chunk:  # filter out keep-alive new chunks
+                        f.write(chunk)
+            return r.ok
         else:
-            return r.text
+            r = requests.request(method, url, **options)
+            logger.debug(r.text)
+            r.raise_for_status()
+            if format == "json":
+                try:
+                    return r.json()
+                except ValueError as e:
+                    logger.error(e)
+                    return r.text
+            else:
+                return r.text
 
     def __get(self, url, params=None, format="json"):
         valid_format = ["json", "xml", "yaml"]
@@ -91,6 +100,9 @@ class Rundeck(object):
                 )
             )
         return self.__request("GET", url, params, format=format)
+
+    def __get_file(self, url, file_path, params=None, format='json'):
+        return self.__request("GET_FILE", url, params, get_file_path=file_path, format=format)
 
     def __post(self, url, params=None, upload_file=None):
         return self.__request("POST", url, params, upload_file)
@@ -108,13 +120,26 @@ class Rundeck(object):
         url = "{}/token/{}".format(self.API_URL, token_id)
         return self.__get(url)
 
-    def get_job_def(self, job_id, format="xml"):
+    def get_job_def(self, job_id, format="json"):
         url = "{}/job/{}".format(self.API_URL, job_id)
         return self.__get(url, format=format)
 
     def get_job_meta(self, job_id):
+        url = "{}/job/{}/meta".format(self.API_URL, job_id)
+        return self.__get(url, format="json")
+
+    def get_job_info(self, job_id):
         url = "{}/job/{}/info".format(self.API_URL, job_id)
-        return self.__get(url, format="xml")
+        return self.__get(url, format="json")
+
+    def import_jobs(self, project, definition, update=False):
+        url = "{}/project/{}/jobs/import".format(self.API_URL, project)
+        params = {
+            'fileformat': 'json',
+            'dupeOption': 'update' if update else 'create',
+            'uuidOption': 'preserve' if update else 'remove'
+        }
+        return self.__post(url, params=params, upload_file=definition)
 
     def create_token(self, user, roles="*", duration=None):
         url = "{}/tokens/{}".format(self.API_URL, user)
@@ -157,18 +182,15 @@ class Rundeck(object):
         url = "{}/projects".format(self.API_URL)
         return self.__get(url)
 
-    def get_project_config(self, project_name):
-        url = "{}/project/{}/config".format(self.API_URL, project_name)
-        return self.__get(url)
-
-    def list_jobs(self, project):
+    def list_jobs(self, project, tags=None):
         url = "{}/project/{}/jobs".format(self.API_URL, project)
-        return self.__get(url)
+        params = {"tags": tags} if tags else None
+        return self.__get(url, params=params)
 
-    def list_all_jobs(self):
+    def list_all_jobs(self, tags=None):
         jobs = []
         for p in self.list_projects():
-            jobs += self.list_jobs(p["name"])
+            jobs += self.list_jobs(p["name"], tags=tags)
         return jobs
 
     def get_job(self, name, project=None):
@@ -302,6 +324,10 @@ class Rundeck(object):
         params = {"groupPath": groupPath}
         return self.__post(url, params=params)
 
+    def get_job_tags(self, job_id):
+        url = "{}/job/{}/tags".format(self.API_URL, job_id)
+        return self.__get(url)
+
     def execution_output_by_id(self, exec_id):
         url = "{}/execution/{}/output".format(self.API_URL, exec_id)
         return self.__get(url)
@@ -332,6 +358,53 @@ class Rundeck(object):
             self.API_URL, project, resource
         )
         return self.__get(url)
+
+    def get_project_history(self, project, job_id=None):
+        url = "{}/project/{}/history".format(self.API_URL, project)
+        params = {"jobIdFilter": job_id} if job_id else None
+        return self.__get(url, params=params)
+
+    def project_archive_export(self, project, file_path):
+        url = f'{self.API_URL}/project/{project}/export'
+        params = {
+            'exportJobs': True,
+            'exportConfigs': True,
+            'exportReadmes': True,
+            'exportAcls': True,
+            'exportComponents.calendars': True,
+            'exportComponents.Schedule Definitions': True,
+            'exportComponents.tours-manager': True,
+            'exportComponents.node-wizard': True,
+            'exportScm': True,
+            'exportWebhooks': True,
+            'whkIncludeAuthTokens': True
+        }
+        return self.__get_file(url, file_path, params=params)
+
+    def project_archive_export_async(self, project):
+        url = f'{self.API_URL}/project/{project}/export/async'
+        params = {
+            'exportJobs': True,
+            'exportConfigs': True,
+            'exportReadmes': True,
+            'exportAcls': True,
+            'exportComponents.calendars': True,
+            'exportComponents.Schedule Definitions': True,
+            'exportComponents.tours-manager': True,
+            'exportComponents.node-wizard': True,
+            'exportScm': True,
+            'exportWebhooks': True,
+            'whkIncludeAuthTokens': True
+        }
+        return self.__get(url, params=params)
+
+    def project_archive_export_async_status(self, project, export_token):
+        url = f'{self.API_URL}/project/{project}/export/status/{export_token}'
+        return self.__get(url)
+
+    def project_archive_export_async_download(self, project, export_token, file_path):
+        url = f'{self.API_URL}/project/{project}/export/download/{export_token}'
+        return self.__get_file(url, file_path)
 
 
 if __name__ == "__main__":
